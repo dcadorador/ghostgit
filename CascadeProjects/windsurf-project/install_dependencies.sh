68e38761d842fe7af7cb7669625aebf0d26386f1
Drew Adorador <drew@simpleclick.com.au>
2025-05-29 18:09:41 +0800

    chore: remove windsurf project and related configuration files

--- CascadeProjects/windsurf-project/install_dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install tkinter for Python 3
-brew install python-tk@3.13
-
-# Create a symbolic link for tkinter
-ln -s /opt/homebrew/opt/python-tk@3.13/lib/python3.13/site-packages/_tkinter.cpython-313-darwin.so /opt/homebrew/opt/python@3.13/Frameworks/Python.framework/Versions/3.13/lib/python3.13/lib-dynload/_tkinter.cpython-313-darwin.so
